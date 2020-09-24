4d902ea11fca64cfac6c1cfa570228f629df70a8
modit jain <moditjain121@gmail.com>
2020-09-24 19:17:25 +0000

    temp removed live display

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -21,8 +21,3 @@ BOARD_SEPOLICY_DIRS += \
     device/corvus/sepolicy/qcom/dynamic \
     device/corvus/sepolicy/qcom/vendor
 endif
-
-ifneq ($(filter msm8226 msm8610 msm8974 msm8909 msm8916 msm8952 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
-BOARD_SEPOLICY_DIRS += \
-    device/corvus/sepolicy/qcom/legacy-vendor
-endif
